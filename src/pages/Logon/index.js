b08e01f08c01bc01dc0484fc7502502f6f9c0fb3
Shima <guilherme.shima91@hotmail.com>
2020-03-29 06:38:49 -0300

    pagina de profile

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
-import './style.css'
+import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
